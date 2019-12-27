cda3c2713505f01fd63bd6efc5c00fe68d86de8f
ahenteti <ahmad.henteti@gmail.com>
2019-12-27 19:50:40 +0100

    rename certificate image names

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,20 +1,23 @@
 import './about.scss';
 
 const CERTIFICATES_OF_COMPLETION = [
-    'img/design-patterns-java-behavioral-certificate.jpg',
-    'img/apache-web-server-install-config-certificate.jpg',
-    'img/build-asynchronous-restful-services-jersey-certificate.jpg',
-    'img/context-dependency-injection-1-1-certificate.jpg',
-    'img/droidcon-boston-2019-session-13-certificate.jpg',
-    'img/getting-started-spring-webflux-certificate.jpg',
-    'img/java-api-websockets-introduction-certificate.jpg',
-    'img/java-persistence-api-21-certificate.jpg',
-    'img/javascript-module-fundamentals-certificate.jpg',
-    'img/javaserver-faces-getting-started-java-ee-certificate.jpg',
-    'img/jcache-fundamentals-certificate.jpg',
-    'img/react-flux-building-applications-certificate.jpg',
-    'img/structuring-javascript-certificate.jpg'
+    'img/certificate-design-patterns-java-behavioral.jpg',
+    'img/certificate-apache-web-server-install-config.jpg',
+    'img/certificate-build-asynchronous-restful-services-jersey.jpg',
+    'img/certificate-context-dependency-injection-1-1.jpg',
+    'img/certificate-droidcon-boston-2019-session-13.jpg',
+    'img/certificate-getting-started-spring-webflux.jpg',
+    'img/certificate-java-api-websockets-introduction.jpg',
+    'img/certificate-java-persistence-api-21.jpg',
+    'img/certificate-javascript-module-fundamentals.jpg',
+    'img/certificate-javaserver-faces-getting-started-java-ee.jpg',
+    'img/certificate-jcache-fundamentals.jpg',
+    'img/certificate-react-flux-building-applications.jpg',
+    'img/certificate-structuring-javascript.jpg'
 ];
+
+const BACK_ARROW = 'back';
+const FORWARD_ARROW = 'forward';
 
 const certificatesOfCompletionArrowBack = document.querySelector(
     '.certificate-of-completion-image-container [name="ios-arrow-back"]'
@@ -23,58 +26,71 @@ const certificatesOfCompletionArrowBack = document.querySelector(
 const certificatesOfCompletionArrowForward = document.querySelector(
     '.certificate-of-completion-image-container [name=ios-arrow-forward]'
 );
-const currentDisplayedCertificate = document.querySelector('.certificate-of-completion-image-container img');
-currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[0];
-currentDisplayedCertificate.dataset.index = 0;
 
-const currentDisplayedCertificateIndex = document.querySelector('.certificate-of-completion-image-container .index');
-currentDisplayedCertificateIndex.innerHTML = `1 / ${CERTIFICATES_OF_COMPLETION.length}`;
+const certificateContainer = document.querySelector('.certificate-container');
 
-const currentDisplayedCertificateLoader = document.querySelector('.certificate-of-completion-image-container .loader');
+const displayedCertificate = document.querySelector('.displayed-certificate');
+displayedCertificate.onload = () => {
+    certificateContainer.style.height = window.getComputedStyle(displayedCertificate).height;
+};
+displayedCertificate.src = CERTIFICATES_OF_COMPLETION[0];
+displayedCertificate.dataset.index = 0;
 
-certificatesOfCompletionArrowBack.addEventListener('click', () => {
-    currentDisplayedCertificate.classList.add('with-loader');
-    currentDisplayedCertificateLoader.style.display = 'block';
-    const nextIndex = calcCertificateNextIndexOnArrowBackClick(currentDisplayedCertificate);
+const displayedCertificateIndex = document.querySelector('.certificate-of-completion-image-container .index');
+displayedCertificateIndex.innerHTML = `1 / ${CERTIFICATES_OF_COMPLETION.length}`;
+
+const loader = document.querySelector('.certificate-of-completion-image-container .loader');
+
+certificatesOfCompletionArrowBack.addEventListener('click', () => handleCertificatesOfCompletionNavigation(BACK_ARROW));
+
+certificatesOfCompletionArrowForward.addEventListener('click', () =>
+    handleCertificatesOfCompletionNavigation(FORWARD_ARROW)
+);
+
+function handleCertificatesOfCompletionNavigation(arrow) {
+    const currentDisplayedCertificate = document.querySelector('.displayed-certificate');
+    const currentHiddenCertificate = document.querySelector('.hidden-certificate');
+    currentDisplayedCertificate.classList.add('lightweight-transparent');
+
+    loader.style.display = 'block';
+    const nextIndex = calcCertificateNextIndex(arrow, currentDisplayedCertificate);
+    const certificateToDisplay = CERTIFICATES_OF_COMPLETION[nextIndex];
     const certificate = new Image();
     certificate.onload = function() {
-        currentDisplayedCertificate.classList.remove('with-loader');
-        currentDisplayedCertificateLoader.style.display = 'none';
-        currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-        currentDisplayedCertificate.dataset.index = nextIndex;
-        currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
-    };
-    certificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-});
+        loader.style.display = 'none';
+        displayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+        currentHiddenCertificate.src = certificateToDisplay;
+        currentHiddenCertificate.dataset.index = nextIndex;
 
-certificatesOfCompletionArrowForward.addEventListener('click', () => {
-    currentDisplayedCertificate.classList.add('with-loader');
-    currentDisplayedCertificateLoader.style.display = 'block';
-    const nextIndex = calcCertificateNextIndexOnArrowForwardClick(currentDisplayedCertificate);
-    const certificate = new Image();
-    certificate.onload = function() {
-        currentDisplayedCertificate.classList.remove('with-loader');
-        currentDisplayedCertificateLoader.style.display = 'none';
-        currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-        currentDisplayedCertificate.dataset.index = nextIndex;
-        currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+        currentHiddenCertificate.classList.remove('displayed-certificate', 'hidden-certificate');
+        currentDisplayedCertificate.classList.remove('displayed-certificate', 'hidden-certificate');
+
+        currentHiddenCertificate.classList.add('displayed-certificate');
+        currentDisplayedCertificate.classList.add('hidden-certificate');
+
+        currentHiddenCertificate.classList.remove('transparent');
+        currentDisplayedCertificate.classList.add('transparent');
+        currentDisplayedCertificate.classList.remove('lightweight-transparent');
     };
-    certificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-});
+    certificate.src = certificateToDisplay;
+}
+
+function calcCertificateNextIndex(arrow, currentDisplayedCertificate) {
+    if (arrow === BACK_ARROW) {
+        return calcCertificateNextIndexOnArrowBackClick(currentDisplayedCertificate);
+    } else {
+        return calcCertificateNextIndexOnArrowForwardClick(currentDisplayedCertificate);
+    }
+}
+
 function calcCertificateNextIndexOnArrowBackClick(currentDisplayedCertificate) {
     const currentDisplayedCertificateIndex = window.parseInt(currentDisplayedCertificate.dataset.index);
-    let nextIndex = currentDisplayedCertificateIndex - 1;
-    if (currentDisplayedCertificateIndex === 0) {
-        nextIndex = CERTIFICATES_OF_COMPLETION.length - 1;
-    }
-    return nextIndex;
+    return (
+        (currentDisplayedCertificateIndex - 1 + CERTIFICATES_OF_COMPLETION.length) % CERTIFICATES_OF_COMPLETION.length
+    );
 }
 
 function calcCertificateNextIndexOnArrowForwardClick(currentDisplayedCertificate) {
     const currentDisplayedCertificateIndex = window.parseInt(currentDisplayedCertificate.dataset.index);
-    let nextIndex = currentDisplayedCertificateIndex + 1;
-    if (currentDisplayedCertificateIndex === CERTIFICATES_OF_COMPLETION.length - 1) {
-        nextIndex = 0;
-    }
-    return nextIndex;
+    return (currentDisplayedCertificateIndex + 1) % CERTIFICATES_OF_COMPLETION.length;
 }
