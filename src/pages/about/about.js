b1ac6660fc0213adee571d6e6dfcc422e8a51e4d
ahenteti <ahmad.henteti@gmail.com>
2019-12-27 15:19:08 +0100

    improve certificate transition

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -26,21 +26,40 @@ const certificatesOfCompletionArrowForward = document.querySelector(
 const currentDisplayedCertificate = document.querySelector('.certificate-of-completion-image-container img');
 currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[0];
 currentDisplayedCertificate.dataset.index = 0;
+
 const currentDisplayedCertificateIndex = document.querySelector('.certificate-of-completion-image-container .index');
 currentDisplayedCertificateIndex.innerHTML = `1 / ${CERTIFICATES_OF_COMPLETION.length}`;
 
+const currentDisplayedCertificateLoader = document.querySelector('.certificate-of-completion-image-container .loader');
+
 certificatesOfCompletionArrowBack.addEventListener('click', () => {
+    currentDisplayedCertificate.classList.add('with-loader');
+    currentDisplayedCertificateLoader.style.display = 'block';
     const nextIndex = calcCertificateNextIndexOnArrowBackClick(currentDisplayedCertificate);
-    currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-    currentDisplayedCertificate.dataset.index = nextIndex;
-    currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+    const certificate = new Image();
+    certificate.onload = function() {
+        currentDisplayedCertificate.classList.remove('with-loader');
+        currentDisplayedCertificateLoader.style.display = 'none';
+        currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
+        currentDisplayedCertificate.dataset.index = nextIndex;
+        currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+    };
+    certificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
 });
 
 certificatesOfCompletionArrowForward.addEventListener('click', () => {
+    currentDisplayedCertificate.classList.add('with-loader');
+    currentDisplayedCertificateLoader.style.display = 'block';
     const nextIndex = calcCertificateNextIndexOnArrowForwardClick(currentDisplayedCertificate);
-    currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
-    currentDisplayedCertificate.dataset.index = nextIndex;
-    currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+    const certificate = new Image();
+    certificate.onload = function() {
+        currentDisplayedCertificate.classList.remove('with-loader');
+        currentDisplayedCertificateLoader.style.display = 'none';
+        currentDisplayedCertificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
+        currentDisplayedCertificate.dataset.index = nextIndex;
+        currentDisplayedCertificateIndex.innerHTML = `${nextIndex + 1} / ${CERTIFICATES_OF_COMPLETION.length}`;
+    };
+    certificate.src = CERTIFICATES_OF_COMPLETION[nextIndex];
 });
 function calcCertificateNextIndexOnArrowBackClick(currentDisplayedCertificate) {
     const currentDisplayedCertificateIndex = window.parseInt(currentDisplayedCertificate.dataset.index);
