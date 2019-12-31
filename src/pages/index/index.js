6fd7d2cf2953d5fb19d0aaa61dedd9c2fd0944d2
ahenteti <ahmad.henteti@gmail.com>
2019-12-31 03:22:49 +0100

    check node version in github actions

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -11,12 +11,12 @@ const $articles = document.querySelector('.articles');
 const $noResult = document.querySelector('.no-result');
 const $searchComponent = document.querySelector('app-search');
 const $searchSelectedOption = $searchComponent.shadowRoot
-    .querySelector('app-search-select')
-    .shadowRoot.querySelector('.select-selected')
-    .querySelector('span');
+  .querySelector('app-search-select')
+  .shadowRoot.querySelector('.select-selected')
+  .querySelector('span');
 const $searchInputContainer = $searchComponent.shadowRoot
-    .querySelector('app-search-input')
-    .shadowRoot.querySelector('.container');
+  .querySelector('app-search-input')
+  .shadowRoot.querySelector('.container');
 const $searchInput = $searchComponent.shadowRoot.querySelector('app-search-input').shadowRoot.querySelector('input');
 
 // ////////////////////////////// //
@@ -34,141 +34,141 @@ animateArticles();
 //        util functions          //
 // ////////////////////////////// //
 function handleTagSelectionEvent(event) {
-    const tag = event.composedPath().find(component => {
-        try {
-            return component.classList.contains('tag');
-        } catch (error) {
-            return false;
-        }
-    });
-    if (tag) {
-        event.preventDefault();
-        const tagValue = tag.innerText;
-        $searchSelectedOption.innerHTML = tagValue;
-        $searchSelectedOption.dispatchEvent(new Event('change'));
-        renderArticles(findArticles());
-        selectTags(tagValue);
+  const tag = event.composedPath().find(component => {
+    try {
+      return component.classList.contains('tag');
+    } catch (error) {
+      return false;
     }
+  });
+  if (tag) {
+    event.preventDefault();
+    const tagValue = tag.innerText;
+    $searchSelectedOption.innerHTML = tagValue;
+    $searchSelectedOption.dispatchEvent(new Event('change'));
+    renderArticles(findArticles());
+    selectTags(tagValue);
+  }
 }
 
 function handleSearchInputKeyupEvent() {
-    const articles = findArticles();
-    if (isEmpty(articles)) {
-        renderNoResult($searchInput.value);
-    } else {
-        renderArticles(articles);
-    }
+  const articles = findArticles();
+  if (isEmpty(articles)) {
+    renderNoResult($searchInput.value);
+  } else {
+    renderArticles(articles);
+  }
 }
 
 function handleSearchInputFocusEvent() {
-    $searchInputContainer.classList.add('focus');
+  $searchInputContainer.classList.add('focus');
 }
 
 function handleSearchInputBlurEvent() {
-    $searchInputContainer.classList.remove('focus');
+  $searchInputContainer.classList.remove('focus');
 }
 
 function selectTags(value) {
-    document.querySelectorAll('.article').forEach(article => {
-        article.shadowRoot.querySelectorAll('app-tag').forEach(tag => {
-            if (tag.shadowRoot.querySelector('span').innerText == value) {
-                tag.classList.add('selected');
-            }
-        });
+  document.querySelectorAll('.article').forEach(article => {
+    article.shadowRoot.querySelectorAll('app-tag').forEach(tag => {
+      if (tag.shadowRoot.querySelector('span').innerText == value) {
+        tag.classList.add('selected');
+      }
     });
+  });
 }
 
 function findArticles() {
-    const select = $searchSelectedOption.innerText;
-    const input = $searchInput.value;
+  const select = $searchSelectedOption.innerText;
+  const input = $searchInput.value;
 
-    const searchKeys = input.toLowerCase().split(' ');
-    const articles = Array.from(ALL_ARTICLES).filter(article => {
-        const articleNameKeys = article.name.toLowerCase().split(' ');
-        const articleTags = article.tags.toLowerCase().split(',');
-        const articleKeys = articleNameKeys.concat(articleTags);
-        var keep = false;
-        searchKeys.forEach(key => {
-            if (input != null && input != '') {
-                if (select == 'all') {
-                    keep = article.searchKey.indexOf(key) > -1;
-                } else {
-                    let keepBasedOnTagValue = false;
-                    article.tags
-                        .toLowerCase()
-                        .split(',')
-                        .forEach(tag => {
-                            if (tag == select) {
-                                keepBasedOnTagValue = true;
-                            }
-                        });
-                    keep = article.searchKey.indexOf(key) > -1 && keepBasedOnTagValue;
-                }
-            } else {
-                if (select == 'all') {
-                    keep = true;
-                } else {
-                    article.tags
-                        .toLowerCase()
-                        .split(',')
-                        .forEach(tag => {
-                            if (tag == select) {
-                                keep = true;
-                            }
-                        });
-                }
-            }
-        });
-        return keep;
-    });
-
-    const articlesByCategory = {};
-    for (const article of articles) {
-        if (!articlesByCategory.hasOwnProperty(article.category)) {
-            articlesByCategory[article.category] = [];
+  const searchKeys = input.toLowerCase().split(' ');
+  const articles = Array.from(ALL_ARTICLES).filter(article => {
+    const articleNameKeys = article.name.toLowerCase().split(' ');
+    const articleTags = article.tags.toLowerCase().split(',');
+    const articleKeys = articleNameKeys.concat(articleTags);
+    var keep = false;
+    searchKeys.forEach(key => {
+      if (input != null && input != '') {
+        if (select == 'all') {
+          keep = article.searchKey.indexOf(key) > -1;
+        } else {
+          let keepBasedOnTagValue = false;
+          article.tags
+            .toLowerCase()
+            .split(',')
+            .forEach(tag => {
+              if (tag == select) {
+                keepBasedOnTagValue = true;
+              }
+            });
+          keep = article.searchKey.indexOf(key) > -1 && keepBasedOnTagValue;
         }
-        articlesByCategory[article.category].push(article);
-    }
-    const sortedArticlesByCategory = Object.entries(articlesByCategory)
-        .sort(([category1], [category2]) => articlesByCategory[category1].length < articlesByCategory[category2].length)
-        .reduce((obj, [key, value]) => Object.assign(obj, { [key]: value }), {});
+      } else {
+        if (select == 'all') {
+          keep = true;
+        } else {
+          article.tags
+            .toLowerCase()
+            .split(',')
+            .forEach(tag => {
+              if (tag == select) {
+                keep = true;
+              }
+            });
+        }
+      }
+    });
+    return keep;
+  });
 
-    return sortedArticlesByCategory;
+  const articlesByCategory = {};
+  for (const article of articles) {
+    if (!articlesByCategory.hasOwnProperty(article.category)) {
+      articlesByCategory[article.category] = [];
+    }
+    articlesByCategory[article.category].push(article);
+  }
+  const sortedArticlesByCategory = Object.entries(articlesByCategory)
+    .sort(([category1], [category2]) => articlesByCategory[category1].length < articlesByCategory[category2].length)
+    .reduce((obj, [key, value]) => Object.assign(obj, { [key]: value }), {});
+
+  return sortedArticlesByCategory;
 }
 
 function delay(fn, ms) {
-    let timer = 0;
-    return function(...args) {
-        clearTimeout(timer);
-        timer = setTimeout(fn.bind(this, ...args), ms || 0);
-    };
+  let timer = 0;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(fn.bind(this, ...args), ms || 0);
+  };
 }
 
 function renderAllArticles() {
-    renderArticles(ALL_ARTICLES_BY_CATEGORY);
+  renderArticles(ALL_ARTICLES_BY_CATEGORY);
 }
 
 function renderArticles(articles) {
-    resetArticles();
-    resetNoResult();
-    Object.keys(articles).forEach(category => {
-        renderCategory(category);
-        let categoryArticlesMarkup = '<div class="articles-by-category">';
-        for (const article of articles[category]) {
-            categoryArticlesMarkup += calcArticleMarkup(article);
-        }
-        categoryArticlesMarkup += '</div>';
-        $articles.insertAdjacentHTML('beforeend', categoryArticlesMarkup);
-    });
+  resetArticles();
+  resetNoResult();
+  Object.keys(articles).forEach(category => {
+    renderCategory(category);
+    let categoryArticlesMarkup = '<div class="articles-by-category">';
+    for (const article of articles[category]) {
+      categoryArticlesMarkup += calcArticleMarkup(article);
+    }
+    categoryArticlesMarkup += '</div>';
+    $articles.insertAdjacentHTML('beforeend', categoryArticlesMarkup);
+  });
 }
 
 function renderCategory(category) {
-    const markup = /* html */ `<app-fancy-title value="${category}"></app-fancy-title>`;
-    $articles.insertAdjacentHTML('beforeend', markup);
+  const markup = /* html */ `<app-fancy-title value="${category}"></app-fancy-title>`;
+  $articles.insertAdjacentHTML('beforeend', markup);
 }
 
 function calcArticleMarkup(article) {
-    return /* html */ `
+  return /* html */ `
   <app-article-card class="article"
     url="${article.url}"
     name="${article.name}"
@@ -179,59 +179,59 @@ function calcArticleMarkup(article) {
 }
 
 function renderNoResult(value) {
-    resetArticles();
-    $noResult.innerHTML = `<span>Sorry, no results for "${value}"</span>`;
+  resetArticles();
+  $noResult.innerHTML = `<span>Sorry, no results for "${value}"</span>`;
 }
 
 function resetNoResult() {
-    $noResult.innerHTML = '';
+  $noResult.innerHTML = '';
 }
 
 function resetArticles() {
-    $articles.innerHTML = '';
+  $articles.innerHTML = '';
 }
 
 function handleLocationSearchParam() {
-    const searchParam = new URLSearchParams(window.location.search);
-    const tag = searchParam.get('tag');
-    if (tag) {
-        $searchSelectedOption.innerHTML = tag;
-        $searchSelectedOption.dispatchEvent(new Event('change'));
-        renderArticles(findArticles());
-        selectTags(tag);
-    } else {
-        renderAllArticles();
-    }
+  const searchParam = new URLSearchParams(window.location.search);
+  const tag = searchParam.get('tag');
+  if (tag) {
+    $searchSelectedOption.innerHTML = tag;
+    $searchSelectedOption.dispatchEvent(new Event('change'));
+    renderArticles(findArticles());
+    selectTags(tag);
+  } else {
+    renderAllArticles();
+  }
 }
 
 function animateArticles() {
-    document.querySelectorAll('app-fancy-title').forEach(checkElementVisibility);
-    document.querySelectorAll('app-article-card').forEach(checkElementVisibility);
+  document.querySelectorAll('app-fancy-title').forEach(checkElementVisibility);
+  document.querySelectorAll('app-article-card').forEach(checkElementVisibility);
 
-    window.addEventListener('scroll', function() {
-        document.querySelectorAll('app-fancy-title').forEach(addCssComeInClassIfVisible);
-        document.querySelectorAll('app-article-card').forEach(addCssComeInClassIfVisible);
-    });
+  window.addEventListener('scroll', function() {
+    document.querySelectorAll('app-fancy-title').forEach(addCssComeInClassIfVisible);
+    document.querySelectorAll('app-article-card').forEach(addCssComeInClassIfVisible);
+  });
 }
 function addCssComeInClassIfVisible(element) {
-    if (element.alreadyVisible()) {
-        element.addComeInClass();
-    }
+  if (element.alreadyVisible()) {
+    element.addComeInClass();
+  }
 }
 
 function checkElementVisibility(element) {
-    if (element.alreadyVisible()) {
-        element.addAlreadyVisibleClass();
-    } else {
-        element.addNotYetVisibleClass();
-    }
+  if (element.alreadyVisible()) {
+    element.addAlreadyVisibleClass();
+  } else {
+    element.addNotYetVisibleClass();
+  }
 }
 
 function isEmpty(map) {
-    for (var key in map) {
-        if (map.hasOwnProperty(key)) {
-            return false;
-        }
+  for (var key in map) {
+    if (map.hasOwnProperty(key)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,166 +13,166 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 let ARTICLES_METADATA = null;
 
 const entries = glob.sync('./src/pages/{index,about,articles}/**/*.js').reduce((acc, filePath) => {
-    const parentFolderName = getParentFolderName(filePath);
-    const fileNameWithoutExtension = getBasenameWithoutExtension(filePath, '.js');
-    if (parentFolderName === fileNameWithoutExtension) {
-        acc[getBasenameWithoutExtension(filePath, '.js')] = filePath;
-    }
-    return acc;
+  const parentFolderName = getParentFolderName(filePath);
+  const fileNameWithoutExtension = getBasenameWithoutExtension(filePath, '.js');
+  if (parentFolderName === fileNameWithoutExtension) {
+    acc[getBasenameWithoutExtension(filePath, '.js')] = filePath;
+  }
+  return acc;
 }, {});
 entries.common = './src/common/common.js';
 entries.components = './src/components/components.js';
 
 const htmlWebpackPlugins = glob.sync('./src/pages/{index,about,articles}/**/*.html').reduce((acc, filePath) => {
-    acc.push(
-        new HtmlWebpackPlugin({
-            filename: path.basename(filePath),
-            template: filePath,
-            chunks: ['components', 'common', getBasenameWithoutExtension(filePath, '.html')],
-            chunksSortMode: 'manual'
-        })
-    );
-    return acc;
+  acc.push(
+    new HtmlWebpackPlugin({
+      filename: path.basename(filePath),
+      template: filePath,
+      chunks: ['components', 'common', getBasenameWithoutExtension(filePath, '.html')],
+      chunksSortMode: 'manual'
+    })
+  );
+  return acc;
 }, []);
 
 module.exports = {
-    entry: entries,
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].[chunkhash].js'
-    },
-    resolve: {
-        modules: [path.resolve('./src'), path.resolve('./node_modules')]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new DeclareComponentsPlugin(),
-        new ManifestPlugin(),
-        ...htmlWebpackPlugins,
-        new InlineChunkManifestHtmlWebpackPlugin(),
-        new ExtractTextPlugin({
-            filename: 'css/[name].[chunkhash].css'
-        }),
-        new webpack.DefinePlugin({
-            ALL_ARTICLES: JSON.stringify(getArticlesMetadata()),
-            ALL_ARTICLES_BY_CATEGORY: JSON.stringify(toArticlesByCategory(getArticlesMetadata())),
-            ALL_TAGS: JSON.stringify(getArticlesTags())
-        }),
-        new CopyWebpackPlugin([{ from: 'src/img', to: 'img' }])
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.s?[ac]ss$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.html$/,
-                use: 'html-loader?interpolate'
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                loader: 'file-loader?name=img/[name].[ext]'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|svg)$/,
-                loader: 'file-loader?name=fonts/[name].[ext]'
-            }
-        ]
-    },
-    node: {
-        fs: 'empty'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    watchOptions: {
-        ignored: ['./src/components/components.js']
-    }
+  entry: entries,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  resolve: {
+    modules: [path.resolve('./src'), path.resolve('./node_modules')]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new DeclareComponentsPlugin(),
+    new ManifestPlugin(),
+    ...htmlWebpackPlugins,
+    new InlineChunkManifestHtmlWebpackPlugin(),
+    new ExtractTextPlugin({
+      filename: 'css/[name].[chunkhash].css'
+    }),
+    new webpack.DefinePlugin({
+      ALL_ARTICLES: JSON.stringify(getArticlesMetadata()),
+      ALL_ARTICLES_BY_CATEGORY: JSON.stringify(toArticlesByCategory(getArticlesMetadata())),
+      ALL_TAGS: JSON.stringify(getArticlesTags())
+    }),
+    new CopyWebpackPlugin([{ from: 'src/img', to: 'img' }])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader?interpolate'
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        loader: 'file-loader?name=img/[name].[ext]'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  watchOptions: {
+    ignored: ['./src/components/components.js']
+  }
 };
 
 function getBasenameWithoutExtension(filePath, extension) {
-    return path.basename(filePath, extension);
+  return path.basename(filePath, extension);
 }
 
 function getParentFolderName(filePath) {
-    return path
-        .dirname(filePath)
-        .split('/')
-        .pop();
+  return path
+    .dirname(filePath)
+    .split('/')
+    .pop();
 }
 
 function getArticlesMetadata() {
-    if (ARTICLES_METADATA === null) {
-        ARTICLES_METADATA = glob.sync('./src/pages/articles/**/metadata.json').reduce((articles, filePath) => {
-            articles.push(JSON.parse(fs.readFileSync(filePath)));
-            return articles;
-        }, []);
+  if (ARTICLES_METADATA === null) {
+    ARTICLES_METADATA = glob.sync('./src/pages/articles/**/metadata.json').reduce((articles, filePath) => {
+      articles.push(JSON.parse(fs.readFileSync(filePath)));
+      return articles;
+    }, []);
 
-        const tagToArticlesMap = new Map();
-        for (const article of ARTICLES_METADATA) {
-            for (const tag of article.tags.split(',')) {
-                if (!tagToArticlesMap.get(tag)) {
-                    tagToArticlesMap.set(tag, new Set());
-                }
-                tagToArticlesMap.get(tag).add(article);
-            }
+    const tagToArticlesMap = new Map();
+    for (const article of ARTICLES_METADATA) {
+      for (const tag of article.tags.split(',')) {
+        if (!tagToArticlesMap.get(tag)) {
+          tagToArticlesMap.set(tag, new Set());
         }
-
-        for (const article of ARTICLES_METADATA) {
-            const relatedArticles = new Set();
-            for (const tag of article.tags.split(',')) {
-                for (const article of tagToArticlesMap.get(tag)) {
-                    relatedArticles.add(article);
-                }
-            }
-            relatedArticles.delete(article);
-            const relatedArticlesWithoutRelatedArticlesProperty = [];
-            for (const article of relatedArticles) {
-                const { relatedArticlesProperty, ...articleWithoutRelatedArticlesProperty } = article;
-                relatedArticlesWithoutRelatedArticlesProperty.push(articleWithoutRelatedArticlesProperty);
-            }
-            article.relatedArticles = relatedArticlesWithoutRelatedArticlesProperty;
-
-            const articleNameKeys = article.name.toLowerCase().split(' ');
-            const articleTags = article.tags.toLowerCase().split(',');
-            const articleKeys = articleNameKeys.concat(articleTags);
-            article.searchKey = articleKeys.join(' ');
-        }
+        tagToArticlesMap.get(tag).add(article);
+      }
     }
-    return ARTICLES_METADATA;
+
+    for (const article of ARTICLES_METADATA) {
+      const relatedArticles = new Set();
+      for (const tag of article.tags.split(',')) {
+        for (const article of tagToArticlesMap.get(tag)) {
+          relatedArticles.add(article);
+        }
+      }
+      relatedArticles.delete(article);
+      const relatedArticlesWithoutRelatedArticlesProperty = [];
+      for (const article of relatedArticles) {
+        const { relatedArticlesProperty, ...articleWithoutRelatedArticlesProperty } = article;
+        relatedArticlesWithoutRelatedArticlesProperty.push(articleWithoutRelatedArticlesProperty);
+      }
+      article.relatedArticles = relatedArticlesWithoutRelatedArticlesProperty;
+
+      const articleNameKeys = article.name.toLowerCase().split(' ');
+      const articleTags = article.tags.toLowerCase().split(',');
+      const articleKeys = articleNameKeys.concat(articleTags);
+      article.searchKey = articleKeys.join(' ');
+    }
+  }
+  return ARTICLES_METADATA;
 }
 
 function toArticlesByCategory(allArticles) {
-    const articlesByCategory = {};
-    for (const article of allArticles) {
-        if (!articlesByCategory.hasOwnProperty(article.category)) {
-            articlesByCategory[article.category] = [];
-        }
-        articlesByCategory[article.category].push(article);
+  const articlesByCategory = {};
+  for (const article of allArticles) {
+    if (!articlesByCategory.hasOwnProperty(article.category)) {
+      articlesByCategory[article.category] = [];
     }
-    const sortedArticlesByCategory = Object.entries(articlesByCategory)
-        .sort(([category1], [category2]) => articlesByCategory[category1].length < articlesByCategory[category2].length)
-        .reduce((obj, [key, value]) => Object.assign(obj, { [key]: value }), {});
+    articlesByCategory[article.category].push(article);
+  }
+  const sortedArticlesByCategory = Object.entries(articlesByCategory)
+    .sort(([category1], [category2]) => articlesByCategory[category1].length < articlesByCategory[category2].length)
+    .reduce((obj, [key, value]) => Object.assign(obj, { [key]: value }), {});
 
-    return sortedArticlesByCategory;
+  return sortedArticlesByCategory;
 }
 
 function getArticlesTags() {
-    const tags = glob.sync('./src/pages/articles/**/metadata.json').reduce((tags, filePath) => {
-        JSON.parse(fs.readFileSync(filePath))
-            .tags.split(',')
-            .forEach(tag => tags.add(tag));
-        return tags;
-    }, new Set());
-    let tagsArray = Array.from(tags);
-    tagsArray.sort();
-    return ['all', ...tagsArray];
+  const tags = glob.sync('./src/pages/articles/**/metadata.json').reduce((tags, filePath) => {
+    JSON.parse(fs.readFileSync(filePath))
+      .tags.split(',')
+      .forEach(tag => tags.add(tag));
+    return tags;
+  }, new Set());
+  let tagsArray = Array.from(tags);
+  tagsArray.sort();
+  return ['all', ...tagsArray];
 }
