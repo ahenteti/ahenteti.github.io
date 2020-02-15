3646ba1abfa481f88fa5b583623e03253b5e2db6
ahenteti <ahmad.henteti@gmail.com>
2020-02-15 14:47:42 +0100

    fix how-to-add-ellipsis-to-text-when-overflow-in-css article

--- src/pages/articles/frontend/how-to-add-ellipsis-to-text-when-overflow-in-css/how-to-add-ellipsis-to-text-when-overflow-in-css.js
+++ src/pages/articles/frontend/how-to-add-ellipsis-to-text-when-overflow-in-css/how-to-add-ellipsis-to-text-when-overflow-in-css.js
@@ -1,2 +1,36 @@
 import 'components/webcomponents/webcomponents.js';
 import 'common/common.js';
+import * as constants from 'common/constants.js';
+
+$(window).on(constants.PAGE_CONTENT_READY_EVENT, () => {
+  const $overflowMultiLineTexts = document.querySelectorAll('.overflow-multi-line');
+  setTimeout(() => $overflowMultiLineTexts.forEach(ellipsizeTextBox), 0);
+});
+
+function ellipsizeTextBox(element) {
+  const maxLines = parseInt(element.dataset.maxLines) || 3;
+  const elementLineHeight = calcElementLineHeight(element);
+  let currentElementLines = calcCurrentElementLines(element, elementLineHeight);
+  const wordArray = element.innerHTML.split(' ');
+  element.style.textAlign = 'justify';
+  while (currentElementLines > maxLines) {
+    wordArray.pop();
+    element.innerHTML = wordArray.join(' ') + ' ...';
+    currentElementLines = calcCurrentElementLines(element, elementLineHeight);
+  }
+}
+
+function calcCurrentElementLines(element, elementLineHeight) {
+  return Math.floor(element.offsetHeight / elementLineHeight);
+}
+
+function calcElementLineHeight(element) {
+  let res = element.style.lineHeight;
+  if (isNaN(parseInt(res))) {
+    res = document.defaultView.getComputedStyle(element).getPropertyValue('line-height');
+  }
+  if (isNaN(parseInt(res))) {
+    res = 20;
+  }
+  return parseInt(res);
+}
